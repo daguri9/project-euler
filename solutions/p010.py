95ea8d776feddaff93f05169a92885d1973f29ae
David Aguilar <jdu@ciencias.unam.mx>
2025-03-28 12:33:18 -0600

    Problem 10 solution

---
+++ solutions/p010.py
@@ -0,0 +1,35 @@
+from math import sqrt, ceil
+import time
+
+def nearest_prime_below(n):
+    if n == 3:
+        return 2
+    k = n - 2 if not n % 2 == 0 else n - 1
+    for i in range(k, 1, -2):
+        if is_prime(i):
+            return i
+
+    return -1
+
+
+def is_prime(n):
+    k = ceil(sqrt(n))
+    for i in range(2, k+1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def compute(n):
+    next_prime = nearest_prime_below(n)
+    total_sum = 0
+    while next_prime != -1:
+        total_sum += next_prime
+        next_prime = nearest_prime_below(next_prime)
+
+    return total_sum
+if __name__ == "__main__":
+    start_time = time.time()
+    print(compute(2_000_000))
+    elapsed_time = time.time() - start_time
+    print(f"Finished in {elapsed_time:.3f} seconds.")
