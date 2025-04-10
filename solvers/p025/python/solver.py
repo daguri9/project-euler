6b4b833c6e2b25a7f26555ccdc85196120b0085a
David Aguilar <jdu@ciencias.unam.mx>
2025-04-09 21:56:23 -0600

    Problem 25 Solution

--- solvers/p025/python/solver.py
+++ solvers/p025/python/solver.py
@@ -1,7 +1,19 @@
 import time
 
+
 def main():
-    pass # TODO
+    preb_fib = 1
+    fib = 2
+    digit_count = 1
+    index = 3
+    while digit_count < 1000:
+        next_fib = fib + preb_fib
+        preb_fib, fib = fib, next_fib
+        digit_count = len(str(fib))
+        index += 1
+
+    print(f"{index = }")
+
 
 if __name__ == "__main__":
     start_time = time.time()
