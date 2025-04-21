1c9e0be95300478d55bd904e4f2fdbc5b922f35f
David Aguilar <jdu@ciencias.unam.mx>
2025-04-21 16:11:36 -0600

    Improve solution algorithm

--- solvers/p042/python/solver.py
+++ solvers/p042/python/solver.py
@@ -1,18 +1,10 @@
 import time
 import csv
+from math import sqrt
 
-
-def is_triangle(word):
-    value = word_value(word)
-    i = 0
-    next_triangle = 1
-    while next_triangle <= value:
-        i += 1
-        next_triangle = int((1 / 2) * i * (i + 1))
-        if value == next_triangle:
-            print(word)
-            return True
-    return False
+def is_triangle(t):
+    n = (1/2)*(sqrt(8*t + 1)-1)
+    return n.is_integer()
 
 
 def word_value(word):
@@ -27,9 +19,11 @@ def main():
         reader = csv.reader(f)
         words = next(reader)
 
+
     count = 0
     for w in words:
-        count += 1 if is_triangle(w) else 0
+        val = word_value(w)
+        count += 1 if is_triangle(val) else 0
 
     print(count)
 
@@ -38,4 +32,4 @@ if __name__ == "__main__":
     start_time = time.time()
     main()
     elapsed_time = time.time() - start_time
-    print(f"Finished in {elapsed_time:.3f} seconds.")
+    print(f"Finished in {elapsed_time:.6f} seconds.")
